513bbc895de0f9c6ff5ae30453b1ba86f93a18c5
2damb <2damb@PC234006.informatica.edu>
2023-11-15 18:04:37 +0000

    Ericcc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.trucable {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.trucable to javafx.fxml;
     exports com.mycompany.trucable;
